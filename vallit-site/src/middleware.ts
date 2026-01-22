5c5549b4afcd00babb81dd63587f7c6ca8914e59
4vyrez <160358722+4vyrez@users.noreply.github.com>
2026-01-22 23:15:11 +0100

    Fix: Exclude API routes from Middleware (Retry)

--- vallit-site/src/middleware.ts
+++ vallit-site/src/middleware.ts
@@ -31,6 +31,15 @@ export const config = {
          * - favicon.ico (favicon file)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - widget (Widget assets)
+         * - static (Static files)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|widget|static|brand|company|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
